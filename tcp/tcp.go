5ba23f34f99f8b132ef4da48e0f334b1c8a71ae7
MikelGv <mikelgaldosv@gmail.com>
2024-04-23 07:42:55 +0200

    AccLoop and handleConnection functions added

--- tcp/tcp.go
+++ tcp/tcp.go
@@ -51,3 +51,36 @@ func (t *TCPCHAN[T]) loop() {
     }
 }
 
+func (t *TCPCHAN[T]) accLoop() {
+    defer func() {
+        t.listener.Close()
+    }()
+
+    for {
+        connect, err := t.listener.Accept()
+
+        if err != nil {
+            log.Println("Error accepting", err)
+            return
+        }
+
+        log.Printf("sender connected %s", connect.RemoteAddr())
+
+        go t.handleConnection(connect)
+
+    }
+}
+
+func (t *TCPCHAN[T]) handleConnection(conn net.Conn) { 
+    for {
+        var messsage T
+
+        if err := gob.NewDecoder(conn).Decode(&messsage); err != nil {
+            log.Println(err)
+            continue
+        }
+
+        t.OutChan <- messsage
+    }
+}
+
