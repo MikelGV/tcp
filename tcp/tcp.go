b884dc4099677339cb3d42b3f4773dd60ecdb283
MikelGv <mikelgaldosv@gmail.com>
2024-04-23 06:38:08 +0200

    tcp loop added

--- tcp/tcp.go
+++ tcp/tcp.go
@@ -1,9 +1,53 @@
 package tcp
 
+import (
+	"encoding/gob"
+	"log"
+	"net"
+)
+
 /* Here goes all the tcp functionalities */
 
 type TCPCHAN[T any] struct {
+    
+    listenAddress string
+    remoteAddress string
+
     InChan chan T
     OutChan chan T
+
+    outBoundCon net.Conn
+    listener net.Listener
+}
+
+func New[T any](lAddr, remoteAddr string) (*TCPCHAN[T], error) {
+    tcp := &TCPCHAN[T]{
+        listenAddress: lAddr,
+        remoteAddress: remoteAddr,
+        InChan: make(chan T, 10),
+        OutChan: make(chan T, 10),
+    }
+
+    ln, err := net.Listen("tcp", lAddr)
+
+    if err != nil {
+        return nil, err
+    }
+
+    tcp.listener = ln
+
+    go tcp.loop()
+
+    return tcp, nil
+}
+
+func (t *TCPCHAN[T]) loop() {
+    for {
+        message := <-t.InChan
+        log.Println("sending message", message)
+        if err := gob.NewEncoder(t.outBoundCon).Encode(&message); err != nil {
+            log.Println(err)
+        }
+    }
 }
 
